a7ad89513f18a6fb316c89fa57ed3dd9001e411c
Mark Shead <mark.shead@xeric.net>
2012-11-30 21:54:24 -0600

    added tomcat 7

---
+++ cookbooks/tomcat7/attributes/default.rb
@@ -0,0 +1,17 @@
+tc7target = "/usr/local"
+##
+set[:tomcat7][:version] = "7.0.26"
+set[:tomcat7][:user] = "tomcat"
+set[:tomcat7][:group] = "tomcat"
+set[:tomcat7][:target] = "/usr/local"
+##
+set[:tomcat7][:port] = 8080
+set[:tomcat7][:ssl_port] = 8443
+set[:tomcat7][:ajp_port] = 8009
+set[:tomcat7][:java_options] = " -Xmx128M -Dajva.awt.headless=true"
+set[:tomcat7][:use_security_manager] = "no"
+##
+set[:tomcat7][:home] = "#{tc7target}/tomcat"
+set[:tomcat7][:base] = "#{tc7target}/tomcat"
+set[:tomcat7][:config_dir] = "#{tc7target}/tomcat/conf"
+set[:tomcat7][:log_dir] = "#{tc7target}/tomcat/logs"
---
+++ cookbooks/tomcat7/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Vladislav Mikhaylov"
+maintainer_email "solarvm@gmail.com"
+license          "AGPL"
+description      "Installs/Configures tomcat7 binary distrib"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.2"
